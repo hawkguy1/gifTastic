37adb6b809295600f016ba2295e4b87beb54d575
Andy Harringa <andy.harringa@gmail.com>
2018-09-11 20:15:23 -0700

    Button update

--- assets/main.js
+++ assets/main.js
@@ -1,37 +1,37 @@
 //-- INSTRUMENT ARRAY --//
-var topics = ["BAGPIPES", "BANJO", "BASSOON", "BUGLE", "CELLO", "CLARINET", "DIDGERIDOO", "DRUMS", "GUITAR", "HARMONICA", "KAZOO", "MARACAS", "MARIMBA", "OBOE", "OCARINA", "ORGAN", "PICCOLO", "SAXOPHONE", "SOUSAPHONE", "TAMBOURINE", "TROMBONE", "TRUMPET", "TUBA", "VIOLIN"]
-console.log(topics)
+var instruments = ["BAGPIPES", "BANJO", "BASSOON", "BUGLE", "CELLO", "CLARINET", "DIDGERIDOO", "DRUMS", "GUITAR", "HARMONICA", "KAZOO", "MARACAS", "MARIMBA", "OBOE", "OCARINA", "ORGAN", "PICCOLO", "SAXOPHONE", "SOUSAPHONE", "TAMBOURINE", "TROMBONE", "TRUMPET", "TUBA", "VIOLIN"]
+console.log("INSTRUMENTS: " + instruments)
 //-- INITIALIZES DOCUMENT --//
 $(document).ready(function () {
     //-- CREATES GIF BUTTONS FROM ARRAY --//
     addInstrumentButtons();
     function addInstrumentButtons() {
         $("#gifButtons").empty();
-        for (var i = 0; i < topics.length; i++) {
+        for (var i = 0; i < instruments.length; i++) {
             var instrumentBtn = $("<button>");
             instrumentBtn.addClass("instruments");
             instrumentBtn.addClass("btn btn-default");
-            instrumentBtn.attr("data-name", topics[i]);
-            instrumentBtn.text(topics[i]);
+            instrumentBtn.attr("data-name", instruments[i]);
+            instrumentBtn.text(instruments[i]);
             $("#gifButtons").append(instrumentBtn);
         }
     }
-    addInstrumentButtons();
+    //-- ADDS NEW INSTRUMENT BUTTONS --//
     $("#addButton").on("click", function (event) {
         event.preventDefault();
         var addInstrument = $("#input").val().trim();
-        if (addInstrument !== "") {
-            topics.push(addInstrument);
+        if (addInstrument !== " ") {
+            instruments.push(addInstrument);
             addInstrumentButtons();
-            $("#input").val("");
+            $("#input").val(" ");
         }
     });
-    $(document).on("click", ".gifButtons", showGiphy);
+    $(document).on("click", ".giphyButtons", showGiphy);
     function showGiphy() {
         var giphyName = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=QeJQJNQ2m3JzWEePVxhm6XDKAajxDxP0&q=" + giphyName + "&limit=10&offset=0&rating=G&lang=en";
         $("#giphyColumn").empty();
-        console.log(giphyName);
+        console.log(showGiphy);
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -54,7 +54,7 @@ $(document).ready(function () {
             }
         });
     }
-    $("#giphyArea").on("click", ".gif", function () {
+    $("#giphyColumn").on("click", ".gif", function () {
         var state = $(this).attr("data-state");
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
