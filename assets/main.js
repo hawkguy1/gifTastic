bbba974ec851500a7a9fa6c1c139a0d260a3b0a6
Andy Harringa <andy.harringa@gmail.com>
2018-09-11 22:12:46 -0700

    Just need to to view Gifs

--- assets/main.js
+++ assets/main.js
@@ -19,34 +19,37 @@ $(document).ready(function () {
     //-- ADDS NEW INSTRUMENT BUTTONS --//
     $("#addButton").on("click", function (event) {
         event.preventDefault();
-        var addInstrument = $("#input").val().trim();
+        var addInstrument = $("#instrumentAdder").val().trim();
         console.log("ADDING INSTRUMENT: " + addInstrument);
         if (addInstrument != " ") {
             instruments.push(addInstrument);
             addInstrumentButtons();
-            $("#instrumentAdder").val();
+            $("#instrumentAdder").val("");
         }
     });
-    $(document).on("click", ".giphyButtons", showGiphy);
+    //-- SHOW GIPHY ACCORDING TO BUTTON CLICKED --//
+    $("#giphyButtons").on("click", ".giphyButtons", showGiphy());
     function showGiphy() {
         var giphyName = $(this).attr("data-name");
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=QeJQJNQ2m3JzWEePVxhm6XDKAajxDxP0&q=" + giphyName + "&limit=10&offset=0&rating=G&lang=en";
         $("#giphyColumn").empty();
+        console.log(giphyName);
         console.log(showGiphy);
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
             var results = response.data;
+            console.log("GIPHY RESULTS: " + results)
             for (var i = 0; i < results.length; i++) {
                 var giphyImage = $("<img>");
+                console.log(giphyImage);
                 giphyImage.attr("src", results[i].images.fixed_height_still.url);
                 giphyImage.attr("data-still", results[i].images.fixed_height_still.url);
                 giphyImage.attr("data-animate", results[i].images.fixed_height.url);
                 giphyImage.addClass("gif");
                 giphyImage.attr("data-state", "still");
-                var newItemdiv = $('<div class="giphyColumn">');
+                var newItemdiv = $('<div id="giphyColumn">');
                 var gifRating = results[i].rating;
                 var divRating = $("<p>").text("RATING: " + gifRating);
                 newItemdiv.append(divRating);
@@ -55,13 +58,14 @@ $(document).ready(function () {
             }
         });
     }
-    $("#giphyColumn").on("click", ".gif", function () {
+
+    $("#giphyArea").on("click", function () {
         var state = $(this).attr("data-state");
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
         }
-        else if (state === "animate") {
+        else {
             $(this).attr("src", $(this).attr("data-still"));
             $(this).attr("data-state", "still");
         }
